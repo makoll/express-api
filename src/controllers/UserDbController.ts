ec5c9709d1e1a8966d7955be62d7df8ff76cee92
Makoto Hirose <hirose.makoto@pathee.com>
2020-03-15 23:01:28 +0900

    refactor: change to repository pattern

--- src/controllers/UserDbController.ts
+++ src/controllers/UserDbController.ts
@@ -1,15 +1,17 @@
-import { JsonController, Param, Body, Get, Post, Put, Delete } from "routing-controllers";
+import { JsonController, Body, Post } from "routing-controllers";
+import { getRepository, Raw } from "typeorm";
 import User from "../entities/User";
 
 @JsonController("/userdbs")
 export class UserDbController {
   @Post("/")
   async post(@Body() user: User) {
+    const userRepository = getRepository(User);
     const new_user = new User();
     new_user.name = "Qiita";
     new_user.age = 25;
-    await new_user.save();
-    const users = await User.find();
+    await userRepository.save(new_user);
+    const users = await userRepository.find();
     return users;
   }
 }
